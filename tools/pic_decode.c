0fa2a12166a02300a1e51c1331fb412720a26dc0
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2022-09-14 00:02:58 +0200

    add stdint types, decode header using gog.com info

--- tools/pic_decode.c
+++ tools/pic_decode.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main() {
   int c;
   FILE *picfile;
   picfile = fopen("LABS.PIC", "rb");
 
-  int count = 0;
+  uint16_t count = 0;
+  c = fgetc(picfile);
+  printf("Looks %s\n", (c == 0x07 || c == 0x0F) ? "valid!" : "sketchy");
+  fgetc(picfile);
+  c = (uint8_t)fgetc(picfile);
+  c += (uint8_t)fgetc(picfile) << 8;
+  printf("%d x ", c);
+  c = (uint8_t)fgetc(picfile);
+  c += (uint8_t)fgetc(picfile) << 8;
+  printf("%d\n", c);
+
   do {
     count++;
     c = fgetc(picfile);

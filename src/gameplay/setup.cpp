c0d4da167569ae129983000aa1ffcea32a64fc5b
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2022-09-22 15:02:20 +0200

    add very beginning of a keymapper

---
+++ src/gameplay/setup.cpp
@@ -0,0 +1,19 @@
+/// #include <SDL.h>
+typedef int SDL_Keysym;
+typedef int SDL_Keycode;
+#include <map>
+#include <vector>
+
+namespace kbd {
+	std::map<SDL_Keysym, SDL_Keycode> keymap;
+	std::vector<std::pair<SDL_Keysym, SDL_Keycode>> keypairs;
+
+	void load_default_keymap() {
+		for (auto& keypair : keypairs) {  // TODO: const
+			keymap[keypair.second] = keypair.first;
+		}
+	}
+
+}  /* namespace kbd */
+
+int main() { kbd::load_default_keymap(); return 0; }
